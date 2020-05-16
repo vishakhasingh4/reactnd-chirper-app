7022d8955d7601653817e712a7bef36079becd9c
vishakhasingh4 <singhvishakha308@gmail.com>
2020-05-17 00:54:27 +0530

    second

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,4 +1,4 @@
-import { SET_AUTHED_USER } from "../actions/authedUser";
+import { SET_AUTHED_USER } from '../actions/authedUser';
 
 
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,15 @@
-import {authedUser} from './authedUser'
-import {tweets} from './tweets'
-import {users} from './users'
 import {combineReducers} from 'redux'
+
+import authedUser from './authedUser'
+import tweets from './tweets'
+import users from './users'
+import {loadingBarReducer} from 'react-redux-loading'
 
 
 export default combineReducers({
     authedUser,
+    users,
     tweets,
-    users
+    loadingBar: loadingBarReducer,
+    
 })
